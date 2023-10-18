99ad0bfb38159388313489f746167e40e84a8db7
David-Skyworker <galyamin.david@yandex.ru>
2023-10-18 15:17:29 +0300

    add mkrc generate and margin handler functions

--- libs/gen_library.py
+++ libs/gen_library.py
@@ -11,14 +11,27 @@ def generate_way(info):
 
         :return: словарь, содержащий полную информацию о сгенерированном перегоне в соответствующим формате для
         сохранения в .ini файл.
-        """
+    """
+    _adjust_margins(info)
 
-    rc_list, rc_coordinates = _generate_rc(info["rc"], info["general"])
+    rc_list, rc_coordinates, mkrc_shift_y = _generate_rc(info["rc"], info["general"])
 
-    mgks_list = _generate_mkrc(info["mkrc"], info["general"], rc_coordinates)
+    mkrc_list = _generate_mkrc(info["mkrc"], info["general"], rc_coordinates)
 
-    return {"rc": rc_list, "mgks": rc_list, "mkrc": rc_list, "uksps": rc_list, "arrow": rc_list, "ind": rc_list}
+    return {"rc": rc_list, "mkrc": mkrc_list, "mgks": rc_list, "uksps": rc_list, "arrow": rc_list, "ind": rc_list}
 
+
+def _adjust_margins(info):
+    """
+        Функция настройки отступов МКРЦ и МГКС от ближайших к ним элементов на оступы от координат,
+        заданных в пользовательском интерфейсе.
+        :param info: dict
+            данные о аттрибутах мкрц необходимые для их отрисовки(width, height etc.)
+    """
+    mkrc_margin = info["mkrc"]["upper_margin"] + int(info["rc"]["height"])
+    mgks_margin = info["mgks"]["upper_margin"] + mkrc_margin + int(info["mkrc"]["height"])
+
+    info["mkrc"]["upper_margin"], info["mgks"]["upper_margin"] =mkrc_margin, mgks_margin
 
 def _generate_rc(info, general):
     """
@@ -54,16 +67,11 @@ def _generate_rc(info, general):
     for i, (name, width) in enumerate(details.items()):
 
         # инициализация временной переменой РЦ для шага генерации
-        rc = ['0', '0', '20', '40', 'Имя', '1', '0', '0', '0', '0', '0', '0', '0', '0', '']
+        rc = ['0', '0', info["height"], width, name, '1', '0', '0', general["set_num"] , '0', '0', '0', str(i + 1)  , '0', '']
 
         # запись параметров РЦ
         rc[0] = str(general["coordinates"][1])              # сдвиг по Y
         rc[1] = str(general["coordinates"][0] + shifts[i])  # сдвиг по X
-        rc[2] = info["height"]
-        rc[3] = width
-        rc[4] = name
-        rc[8] = general["set_num"]                       # номер комплекта
-        rc[12] = str(i + 1)                              # номер РЦ
 
         if i + 1 not in [1, info["num"]]:
             # настройка небоковых РЦ
@@ -84,7 +92,7 @@ def _generate_rc(info, general):
         coordinates.append(int(rc[1]))
         generated_rc.append(';'.join(rc))
 
-    return generated_rc, coordinates
+    return generated_rc, coordinates, info["height"]
 
 
 def _generate_mkrc(info, general, coordinates):
@@ -93,63 +101,63 @@ def _generate_mkrc(info, general, coordinates):
     :param info: dict
         данные о аттрибутах мкрц необходимые для их отрисовки(width, height etc.)
 
-    :return: список, содержащий результат генерации РЦ и список координат РЦ
+    :return: список, содержащий результат генерации МКРЦ
     """
+    if info["num"] <= 0:
+        return []
 
-    if info["start_rc"]:
-        if general["direct"]:
+    if general["direct"] == 0:
+        if info["start_rc"] == 0:
             mkrc_coordinates = coordinates[1::2]
         else:
             mkrc_coordinates = coordinates[2::2]
     else:
-        if general["direct"]:
+        if info["start_rc"] == 0:
             mkrc_coordinates = coordinates[::2]
         else:
-            mkrc_coordinates = coordinates[1::2]
+            mkrc_coordinates = coordinates[1:-1:2]
 
-    rc_edges = [coordinates[0], coordinates[-1]]
+    if general["direct"] == 0:
+        rc_edges = [coordinates[1], coordinates[-1]]
+    else:
+        rc_edges = [coordinates[0], coordinates[-2]]
+
     mkrc_coordinates = mkrc_coordinates[:info["num"]]
 
-    for i, coordinate in enumerate(mkrc_coordinates):
+    generated_mkrc = []
+    for i, coordinate_x in enumerate(mkrc_coordinates):
 
-        mkrc = ['0', '0', '20', '20', '0', '0', '0', '0', '0', '0', '1', '0', '']
 
-        mkrc[1] = str(general["coordinates"][0] + shifts[i])  # сдвиг по X
-        mkrc[2] = info["height"]
-        mkrc[3] = info["width"]
-        mkrc[8] = general["set_num"]
+        mkrc = ['0', '0', info["height"], info["width"], '0', '0', '0', '0', general["set_num"], str(i + 1), '2', '0', '']
+        mkrc[0] = str(general["coordinates"][1] + info["upper_margin"])  # сдвиг по Y
 
         # 9 - номер мкрц и 10 - привязка к рц
 
-        if coordinate in rc_edges:
+        # одиночная МКРЦ
+        if coordinate_x in rc_edges:
+            shift_x = round(int(info["width"]) / 2)
+            mkrc[1] = str(coordinate_x - shift_x)
 
+            generated_mkrc.append(';'.join(mkrc))
+        else:
+            mkrc_1 = mkrc.copy()
+            mkrc_1[10] = "1"
 
-        # запись параметров РЦ
-        mkrc[0] = str(general["coordinates"][1])  # сдвиг по Y
+            mkrc_2 = mkrc.copy()
 
+            x = int(coordinate_x)
+            left_x = str(x - int(info["width"]) - 1)
+            right_x = str(x + 1)
 
-        mkrc[8] = general["set_num"]  # номер комплекта
-        mkrc[12] = str(i + 1)  # номер РЦ
-
-        if i + 1 not in [1, info["num"]]:
-            # настройка небоковых РЦ
-            rc[11] = str(int(general["reserved"]))  # резервирование
-        elif i + 1 == info["num"]:
-            # настройка увязки конечной РЦ
-            rc[9] = "64"
-            rc[13] = "2"
-        elif i == 0:
-            # настройка увязки начальной РЦ
-            if general["interface"]:
-                # цифровой интерфейс
-                rc[13] = "3"
+            if general["direct"] == 0:
+                mkrc_1[1], mkrc_2[1] = left_x, right_x
             else:
-                # релейный интерфейс
-                rc[13] = "1"
+                mkrc_1[1], mkrc_2[1] = right_x, left_x
 
-        generated_rc.append(';'.join(rc))
+            generated_mkrc.append(';'.join(mkrc_1))
+            generated_mkrc.append(';'.join(mkrc_2))
 
-    return "None"
+    return generated_mkrc
 
 
 # def _generate_mgks(info, general, coordinates):
